b3f7690b6eadeff326590394a3a7bcf7c8ceff2e
syahridomualim <syahridomualim@gmail.com>
2022-09-18 11:48:27 +0700

    add some changes

---
+++ src/app/model/vehicle.ts
@@ -0,0 +1,10 @@
+export interface Vehicle {
+    registeredNumber: number;
+    owner: string;
+    addresses: string,
+    brand: string,
+    productionYear: number,
+    cylinderCapacity: number,
+    color: string,
+    fuel: string,
+}
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  apiUrl: 'https://daef-114-10-26-92.ngrok.io'
 };
