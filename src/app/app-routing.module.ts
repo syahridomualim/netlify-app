877cab520fa06c5f25e8f2ede76e1080aea99b3b
syahridomualim <syahridomualim@gmail.com>
2022-09-18 13:10:38 +0700

    add some changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './ui/home/home.component';
 
 const routes: Routes = [
-  { path: '/home', component: HomeComponent },
+  { path: 'home', component: HomeComponent },
   { path: '/', redirectTo: '/home', pathMatch: 'full' }
 ];
 
